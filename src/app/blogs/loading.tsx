46d359024c4f9b50ec88a4a4331372a358399b96
shahin2525 <shahin451999@gmail.com>
2024-10-28 22:25:34 -0700

    add loading ui card

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,20 @@
+import LoadingCard from "@/components/ui/LoadingCard";
+import { Blog } from "@/types";
+import React from "react";
+
+const loading = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-4">
+        {blogs.map((blog: Blog) => (
+          <LoadingCard key={blog.id}></LoadingCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default loading;
